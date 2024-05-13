af924c71b81295e09a42f08ac04761c73564b21d
nn-rr-97 <147983043+nn-rr-97@users.noreply.github.com>
2024-05-13 11:36:38 +1000

    update sql learning notes

--- sql_cheatsheet.py
+++ sql_cheatsheet.py
@@ -18,3 +18,21 @@ SELECT column1, column2, column3, ...
 FROM another_table_name
 WHERE condition;
 
+# update
+UPDATE Customers
+SET ContactName = 'Alfred Schmidt', City = 'Frankfurt'
+WHERE CustomerID = 1;
+
+# delete
+DELETE FROM table_name
+WHERE condition;
+
+
+
+# cross join - cartesian product of two or more tables, match each row of one table to every other row of another table
+# when using with WHERE clause, it acts as INNER JOIN
+SELECT * FROM table1
+CROSS JOIN table2;
+
+
+
